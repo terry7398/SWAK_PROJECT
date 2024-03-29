f86b14f2480d69ed7594feb5899d59a83ca5b9e6
Jisung Song <128297715+terry7398@users.noreply.github.com>
2024-03-30 01:02:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 import pandas as pd
 import time
@@ -6,8 +7,8 @@ from io import StringIO
 
 st.header("수학동아리 :blue[방탈출]")
 
-with open("./data.json","r",encoding="utf8") as f:
-    data = json.load(f)
+with open("./data.json","r",encoding="utf-8") as f:
+    a_Data = json.load(f)
 
 if st.button("Refresh"):
     st.rerun()
@@ -19,18 +20,13 @@ def load_ChatData():
         chatData = json.load(f)
     return chatData
 
-def load_Data():
-    with open("./data.json", encoding="utf-8") as f:
-        data = json.load(f)
-    return data
-
-def save(choice, data = data):
+def save(choice, data_ = a_Data):
     if choice == 1:
         with open("./data.json","w",encoding="UTF-8") as f:
-            json.dump(data,f,ensure_ascii=False, indent=4)
+            json.dump(data_,f,ensure_ascii=False, indent=4)
     elif choice == 2:
         with open("./chat_data.json","w", encoding="utf-8") as f:
-            json.dump(data,f,ensure_ascii=False, indent=4)
+            json.dump(data_,f,ensure_ascii=False, indent=4)
 
 chat_load = False
 story_load = False
@@ -41,7 +37,7 @@ with story:
     chat_load = False
     if "StoryText" not in st.session_state:
         st.session_state['StoryText'] = ""
-    story_text = data["Story"]
+    story_text = a_Data['Story']
     with st.expander("스토리 내용"):
         st.write(story_text)
     with st.expander("스토리 수정하기"):
@@ -55,7 +51,7 @@ with story:
                 if st.session_state['StoryText'] == "":
                     st.error("스토리를 입력해 주세요")
                 else:
-                    data["Story"] = st.session_state['StoryText']
+                    a_Data["Story"] = st.session_state['StoryText']
                     save(1)
                     st.success("성공적으로 수정되었습니다",icon="✅")
                     st.rerun()
@@ -65,15 +61,15 @@ with problem:
     story_load = False
     i = 0
     with st.expander("문제  ⤵"):
-        for row in data["Problem"].keys():
+        for row in a_Data["Problem"].keys():
             if st.button(f"{row}"):
                 with st.container():
                     try: 
-                        data_ = {"문제 내용": data["Problem"][row]["ProblemContent"],"답 형식":data["Problem"][row]["AnswerType"],"예시":data["Problem"][row]["Example"]}
+                        data_ = {"문제 내용": a_Data["Problem"][row]["ProblemContent"],"답 형식":a_Data["Problem"][row]["AnswerType"],"예시":a_Data["Problem"][row]["Example"]}
                         data__ = pd.Series(data_)
                         st.write(data__)
                     except:
-                        data_ = {"문제 내용": data["Problem"][row]["ProblemContent"],"답 형식":data["Problem"][row]["AnswerType"]}
+                        data_ = {"문제 내용": a_Data["Problem"][row]["ProblemContent"],"답 형식":a_Data["Problem"][row]["AnswerType"]}
                         data__ = pd.Series(data_)
                         st.write(data__)
             i+=1
@@ -102,7 +98,7 @@ with problem:
                     if st.session_state['AnswerType'] == "" or st.session_state['ProblemName'] == "" or st.session_state['ProblemContent'] == "":
                         st.error("문제를 입력해 주세요")
                     else:
-                        data["Problem"][st.session_state['ProblemName']] = {
+                        a_Data["Problem"][st.session_state['ProblemName']] = {
                                 "ProblemContent" : st.session_state['ProblemContent'], 
                                     "AnswerType" : st.session_state['AnswerType'],
                                     "Example" : st.session_state['Example']
@@ -111,7 +107,7 @@ with problem:
                         st.success("성공적으로 추가되었습니다",icon="✅")
                         st.rerun()
 
-    with st.expander("JSON파일로 데이터 수정/다운로드 하기"):
+    with st.expander("JSON 데이터 다운로드"):
         with open("./data.json",encoding="utf-8") as f:
             st.download_button(
                     label="Download JSON Data File",
@@ -125,14 +121,7 @@ with problem:
                 file_name="chat_data.json",
             )
             
-        uploaded_file = st.file_uploader("Upload JSON data file")
 
-        if uploaded_file is not None:
-            json_data = json.dumps(uploaded_file.getvalue().decode("utf-8"),ensure_ascii=False)
-            with open("./data.json", "w",encoding="utf-8") as f:
-                json.dump(json_data, f,ensure_ascii=False, indent=4)
-                st.rerun()
-            
 with comment:
     problem_load = False
     story_load = False
@@ -146,13 +135,12 @@ with comment:
         if chat[0] == "!":
             try:
                 chat_data["chat"].remove(chat[1:])
-                st.success(f"{chat}이 리스트에서 삭제됨")
-                save(2,data=chat_data)
+                save(2,data_=chat_data)
                 st.rerun()
             except Exception as e:
                 st.error(f"{chat}이 리스트에서 삭제할 수 없습니다\n\n Error:{e}")
         else:
             chat_data["chat"].append(chat)
             messages.chat_message("user").write(chat)
-            save(2,data=chat_data)
-        
+            save(2,data_=chat_data)     
+

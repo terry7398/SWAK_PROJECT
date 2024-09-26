9393454c5f081084dde272210055bd18a353b6ec
JISUNG-DESKTOP\jsson <jssong7398@gmail.com>
2024-09-26 17:52:48 +0900

    화면 비 수정

--- swak-cafe/src/App.js
+++ swak-cafe/src/App.js
@@ -1,10 +1,16 @@
-import React from "react";
+import { React, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import QuestionPage from "./pages/QuestionPage";
 import "./App.css";
 
 const App = () => {
+  useEffect(() => {
+    const vh = window.innerHeight * 0.01;
+    const vw = window.innerWidth * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+    document.documentElement.style.setProperty("--vw", `${vw}px`);
+  }, []);
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />

ae0d8654c6e05c4d1a03375ff1fb776493c8206d
JISUNG-DESKTOP\jsson <jssong7398@gmail.com>
2024-12-18 23:28:33 +0900

    . Vercel Analytics 추가

--- swakspeedquiz/src/app/layout.js
+++ swakspeedquiz/src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import "../style/Global.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "스피드 퀴즈 레이스",
@@ -10,6 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
+      <Analytics />
     </html>
   );
 }
